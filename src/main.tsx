25473fab8b2dedafb2790d4a351ffdf9c8cbfe3b
Mateus Aranha Martins <91752012+mateusaranha@users.noreply.github.com>
2026-09-07 11:02:05 -0300

    Load progressive library styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./videoStatus.css";
+import "./libraryProgressive.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
