71321dc162ceb0d866df1e1f8c8bb51f9737bf8e
Mateus Aranha Martins <91752012+mateusaranha@users.noreply.github.com>
2026-09-08 11:37:17 -0300

    Load explicit collapsed group stack styles

--- src/HymnGroupList.tsx
+++ src/HymnGroupList.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useId, useMemo, useState } from "react";
 import type { ReactNode } from "react";
+import "./hymnGroupStack.css";
 import { hymnLayout } from "./hymnState";
 import type { Hymn } from "./hymnState";
 
